e70eb5ba6c83614eaa59f68de1b05702cd3585ef
sadan <117494111+sadan4@users.noreply.github.com>
2024-08-06 21:23:58 -0400

    create plugin

--- index.tsx
+++ index.tsx
@@ -1,12 +1,35 @@
 import definePlugin from "@utils/types";
-
+import { findByCodeLazy } from "@webpack";
+const normalMessageComponent = findByCodeLazy(".USER_MENTION)");
 export default definePlugin({
-    name: "TextProfileView",
+    name: "FullUserInChatbox",
     description: "",
     authors: [
         {
             name: "sadan",
             id: 521819891141967883n
         }
-    ]
+    ],
+
+    patches: [
+        {
+            find: "UNKNOWN_ROLE_PLACEHOLDER]",
+            replacement: {
+                match: /(hidePersonalInformation.*?)return/,
+                replace: "$1return $self.patchChatboxMention(arguments[0]);"
+            }
+        }
+    ],
+
+    patchChatboxMention(props) {
+        return normalMessageComponent({
+            // this seems to be constant
+            className: "mention",
+            userId: props.id,
+            channelId: props.channelId,
+            //this seems to always be false/undef
+            inlinePreview: undefined
+        })
+    },
 })
+
